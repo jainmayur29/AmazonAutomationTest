b820aef63b4e148a2f4aba24995d0639e1fe0a5c
Mayur Jain <mayurjain.mj29@gmail.com>
2024-06-18 10:08:55 +0530

    Added AddToCart testcase

---
+++ Amazon/src/test/java/com/amazon/testcases/AddToCart.java
@@ -0,0 +1,25 @@
+package com.amazon.testcases;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+import com.amazon.pages.*;
+
+import com.amazon.pages.basePage;
+import com.amazon.utilities.ReadingPropertiesFile;
+
+public class AddToCart extends basePage{
+
+    @Test
+    public void createAccountTest()
+    {
+        driver.findElement(By.id("nav-link-accountList")).click();
+        driver.findElement(createAccountPage.btn_createAccount).click();
+        driver.findElement(By.id("ap_customer_name")).sendKeys(ReadingPropertiesFile.getProperty("yourName"));
+        driver.findElement(By.id("ap_phone_number")).sendKeys(ReadingPropertiesFile.getProperty("mobileNumber"));
+        driver.findElement(By.id("ap_email")).sendKeys(ReadingPropertiesFile.getProperty("emailAddress"));
+        driver.findElement(By.id("ap_password")).sendKeys(ReadingPropertiesFile.getProperty("password"));
+        //driver.findElement(By.id("continue")).click();
+        System.out.println("Account created successfully");
+
+    }
+}
